30487bb4546001c213fc5e007265e8cdfaf90c46
zhoujian <zhoujian_ys@163.com>
2021-02-22 23:09:59 +0800

    juc学习demo

---
+++ src/main/java/com/study/JucTest.java
@@ -0,0 +1,27 @@
+package com.study;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * @author zhoujian
+ * @since 2021/2/22 22:47
+ */
+public class JucTest {
+    public static void main(String[] args) {
+        CyclicBarrier barrier = new CyclicBarrier(20, () -> {
+            System.out.println("满人，发车");
+        });
+
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                try {
+                    barrier.await();
+                    System.out.println(Thread.currentThread().getName());
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }, i + 1 + "").start();
+        }
+    }
+}
